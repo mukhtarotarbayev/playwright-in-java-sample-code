f4ec831db97a5a424fa1689790710c225bfa05e4
Mukhtar Otarbayev <mukhtar.otarbayev@rakuten.com>
2024-12-23 07:57:27 +0900

    Update ASimplePlaywrightTest.java
    initial commit

--- src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
@@ -1,56 +1,15 @@
 package com.serenitydojo.playwright;
 
-import com.microsoft.playwright.*;
-import org.junit.jupiter.api.*;
-
-import java.util.Arrays;
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class ASimplePlaywrightTest {
 
-    private static Playwright playwright;
-    private static Browser browser;
-    private static BrowserContext browserContext;
-
-    Page page;
-
-    @BeforeAll
-    public static void setUpBrowser() {
-        playwright = Playwright.create();
-        browser = playwright.chromium().launch(
-                new BrowserType.LaunchOptions()
-                        .setHeadless(true)
-                        .setArgs(Arrays.asList("--no-sandbox","--disable-extensions","--disable-gpu"))
-        );
-    }
-
-    @BeforeEach
-    public void setUp() {
-        browserContext = browser.newContext();
-        page = browserContext.newPage();
-    }
-
-    @AfterAll
-    public static void tearDown() {
-        browser.close();
-        playwright.close();
-    }
-
     @Test
     void shouldShowThePageTitle() {
-        page.navigate("https://practicesoftwaretesting.com");
-        String title = page.title();
-        Assertions.assertTrue(title.contains("Practice Software Testing"));
+        // TODO: Write your first playwright test here
     }
-
-    @Test
-    void shouldShowSearchTermsInTheTitle() {
-        page.navigate("https://practicesoftwaretesting.com");
-        page.locator("[placeholder=Search]").fill("Pliers");
-        page.locator("button:has-text('Search')").click();
-
-        int matchingProductCount = page.locator(".card-title").count();
-
-        Assertions.assertTrue(matchingProductCount > 0);
-    }
-
 }
